51ee90b9de7543956fcae12199f2961ae09571ba
Zakir  Hossain <hossain.zakir@ssgbd.com>
2023-09-02 16:26:31 +0600

    update

---
+++ resources/js/backend/pages/PageSettings/About/Attribute.jsx
@@ -0,0 +1,84 @@
+import React, {useState} from 'react';
+import {ucFirst} from "../../../../lib/helper.js";
+
+function Attribute({info}) {
+    const {title, bg, icon, hoverIcon, brief} = info
+    const [inputTitle, setTitle] = useState(title || "")
+    const [inputIcon, setIcon] = useState(icon || "")
+    const [inputHoverIcon, setHoverIcon] = useState(hoverIcon || "")
+    const [inputBrief, setBrief] = useState(brief || "")
+
+    const handleInputChange = (event) => {
+        const { name, value, files } = event.target;
+        console.log({ [name]: value });
+
+        if (name.includes('title')){
+            setTitle(value)
+        } else if (name.includes('icon')){
+            setIcon(files[0])
+        } else if (name.includes('secondary_icon')){
+            setHoverIcon(files[0])
+        } else if (name.includes('brief')){
+            setBrief(value)
+        }
+    }
+
+    return (
+        <div className="card">
+            <div className="card-header">
+                <h4>{ucFirst(title || "")}</h4>
+            </div>
+            <div className="card-body">
+                <div className="form-group">
+                    <label>Attribute Title
+                    </label>
+                    <input
+                        className="form-control"
+                        value={inputTitle}
+                        placeholder="Project count"
+                        type="text"
+                        name={`${title.replaceAll(" ", "_")}_title`}
+                        onChange={e=>handleInputChange(e)}
+                    />
+                </div>
+                <div className="form-group mt-2">
+                    <label>Attribute Icon
+                    </label>
+                    <input
+                        type="file"
+                        className="form-control"
+                        name={`${title.replaceAll(" ", "_")}_icon`}
+                        onChange={e=>handleInputChange(e)}
+                    />
+
+                </div>
+                <div className="form-group mt-2">
+                    <label>Attribute Secondary Icon
+                    </label>
+                    <input
+                        type="file"
+                        className="form-control"
+                        name={`${title.replaceAll(" ", "_")}_secondary_icon`}
+                        onChange={e=>handleInputChange(e)}
+                    />
+
+                </div>
+                <div className="form-group mt-2">
+                    <label>Attribute Brief
+                    </label>
+                    <textarea
+                        className="form-control"
+                        name={`${title.replaceAll(" ", "_")}_brief`}
+                        id="textarea" cols="30"
+                        rows="3"
+                        placeholder="Brief"
+                        value={inputBrief}
+                        onChange={e=>handleInputChange(e)}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Attribute;
--- resources/js/backend/pages/PageSettings/About/Index.jsx
+++ resources/js/backend/pages/PageSettings/About/Index.jsx
@@ -4,194 +4,148 @@ import {fetchAboutSettings} from "../../../../featurs/Settings/SettingsSlice.js"
 import HeaderMeta from "../../../../ui/HeaderMeta.jsx";
 import Breadcrumb from "../../../components/Breadcrumb/Index.jsx";
 import {ucFirst, uid} from "../../../../lib/helper.js";
+import InputFile from "../../../components/inputFile/Index.jsx"
+import Attribute from "./Attribute.jsx";
 
 function Index(props) {
-const {isLoading, aboutSetting} = useSelector(state => state.generalSettings);
-const dispatch = useDispatch();
+    const {isLoading, aboutSetting} = useSelector(state => state.generalSettings);
+    const dispatch = useDispatch();
 
-const breadcrumb = [{
-name: "Dashboard", url: "/bsl/admin"
-}, {
-name: "Page Settings", url: "/bsl/admin/page-settings"
-}, {
-name: "About Settings", url: null
-}];
+    const breadcrumb = [{
+        name: "Dashboard", url: "/bsl/admin"
+    }, {
+        name: "Page Settings", url: "/bsl/admin/page-settings"
+    }, {
+        name: "About Settings", url: null
+    }];
 
-const [title, setTitle] = useState("");
-const [brief, setBrief] = useState("");
-const [clients, setClients] = useState("");
-const [project, setProject] = useState("");
-const [attributes, setAttributes] = useState("");
-
-const requestHandler = (e) => {
-e.preventDefault();
-}
-
-useEffect(() => {
-if (aboutSetting) {
-setTitle(aboutSetting?.title)
-setBrief(aboutSetting?.brief)
-setClients(aboutSetting?.clients)
-setProject(aboutSetting?.project)
-setAttributes(aboutSetting?.attributes)
-}
-}, [aboutSetting])
+    const [aboutSTitle, setTitle] = useState("");
+    const [aboutSBrief, setBrief] = useState("");
+    const [clients, setClients] = useState("");
+    const [project, setProject] = useState("");
+    const [attributes, setAttributes] = useState([]);
 
 
-useEffect(() => {
-dispatch(fetchAboutSettings())
-}, []);
-return (
-<>
-<HeaderMeta
-    title="About Settings"
-    url="/bsl/admin/page-settings/about"
-/>
-<Breadcrumb list={breadcrumb}/>
-<div className="container-fluid">
-    <div className="row">
-        <div className="col-md-12">
-            <div className="card">
-                <div className="card-header">
-                    <h4 className="card-title">About Settings</h4>
-                </div>
-                <div className="card-body">
+    const requestHandler = (e) => {
+        e.preventDefault();
+    }
 
-                    <form className="form-profile" onSubmit={requestHandler}>
-                        <div className="row">
-                            <div className="col-md-12">
-                                <div className="form-group">
-                                    <label>Title</label>
-                                    <input
-                                        className="form-control"
-                                        value={title}
-                                        onChange={e => setTitle(e.target.value)}
-                                        placeholder="About title"
-                                        type="text"
-                                    />
-                                </div>
+    useEffect(() => {
+        if (aboutSetting) {
+            setTitle(aboutSetting?.title)
+            setBrief(aboutSetting?.brief)
+            setClients(aboutSetting?.clients)
+            setProject(aboutSetting?.project)
+            setAttributes(aboutSetting?.attributes)
+        }
+    }, [aboutSetting])
+
+
+    useEffect(() => {
+        dispatch(fetchAboutSettings())
+    }, []);
+    return (
+        <>
+            <HeaderMeta
+                title="About Settings"
+                url="/bsl/admin/page-settings/about"
+            />
+            <Breadcrumb list={breadcrumb}/>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="card">
+                            <div className="card-header">
+                                <h4 className="card-title">About Settings</h4>
                             </div>
-                            <div className="col-md-6">
-                                <div className="form-group">
-                                    <label>Clients</label>
-                                    <input
-                                        className="form-control"
-                                        value={clients}
-                                        onChange={e => setClients(e.target.value)}
-                                        placeholder="Clients count"
-                                        type="number"
-                                    />
-                                </div>
-                            </div>
-                            <div className="col-md-6">
-                                <div className="form-group">
-                                    <label>Project</label>
-                                    <input
-                                        className="form-control"
-                                        value={project}
-                                        onChange={e => setProject(e.target.value)}
-                                        placeholder="Project count"
-                                        type="number"
-                                    />
-                                </div>
-                            </div>
+                            <div className="card-body">
 
-                            <div className="col-md-12">
-                                <div className="form-group">
-                                    <label>Brief</label>
-                                    <textarea
-                                        className="form-control"
-                                        name="textarea"
-                                        id="textarea" cols="30"
-                                        rows="3"
-                                        placeholder="Brief"
-                                        value={brief}
-                                        onChange={e => setBrief(e.target.value)}
-                                    />
-                                </div>
-                            </div>
-
-                            <div className="col-md-12">
-                                <div className="row">
-                                    {attributes && Array.from(attributes).map(attrbute => {
-                                        return (
-                                            <div className="col-md-6" key={uid()}>
-                                                <div className="card">
-                                                    <div className="card-header">
-                                                        <h4>{ucFirst(attrbute?.title)}</h4>
-                                                    </div>
-                                                    <div className="card-body">
-                                                        <div className="form-group">
-                                                            <label>Attribute Title
-                                                            </label>
-                                                            <input
-                                                                className="form-control"
-                                                                value={project}
-                                                                onChange={e => setProject(e.target.value)}
-                                                                placeholder="Project count"
-                                                                type="number"
-                                                            />
-                                                        </div>
-                                                        <div className="form-group mt-2">
-                                                            <label>BG Colour
-                                                            </label>
-                                                            <input
-                                                                className="form-control"
-                                                                value={project}
-                                                                onChange={e => setProject(e.target.value)}
-                                                                placeholder="Project count"
-                                                                type="text"
-                                                            />
-                                                        </div>
-                                                        <div className="form-group mt-2">
-                                                            <label>Attribute Icon
-                                                            </label>
-
-                                                        </div>
-                                                        <div className="form-group mt-2">
-                                                            <label>Attribute Secondary Icon
-                                                            </label>
-
-                                                        </div>
-                                                        <div className="form-group mt-2">
-                                                            <label>Attribute Brief
-                                                            </label>
-                                                            <textarea
-                                                                className="form-control"
-                                                                name="textarea"
-                                                                id="textarea" cols="30"
-                                                                rows="3"
-                                                                placeholder="Brief"
-                                                                value={brief}
-                                                                onChange={e => setBrief(e.target.value)}
-                                                            />
-                                                        </div>
-                                                    </div>
-                                                </div>
+                                <form className="form-profile" onSubmit={requestHandler}>
+                                    <div className="row">
+                                        <div className="col-md-12">
+                                            <div className="form-group">
+                                                <label>Title</label>
+                                                <input
+                                                    className="form-control"
+                                                    value={aboutSTitle || ""}
+                                                    onChange={e => setTitle(e.target.value)}
+                                                    placeholder="About title"
+                                                    type="text"
+                                                />
                                             </div>
-                                        )
-                                    })}
-                                </div>
-                            </div>
+                                        </div>
+                                        <div className="col-md-6">
+                                            <div className="form-group">
+                                                <label>Clients</label>
+                                                <input
+                                                    className="form-control"
+                                                    value={clients || ""}
+                                                    onChange={e => setClients(e.target.value)}
+                                                    placeholder="Clients count"
+                                                    type="number"
+                                                />
+                                            </div>
+                                        </div>
+                                        <div className="col-md-6">
+                                            <div className="form-group">
+                                                <label>Project</label>
+                                                <input
+                                                    className="form-control"
+                                                    value={project || ""}
+                                                    onChange={e => setProject(e.target.value)}
+                                                    placeholder="Project count"
+                                                    type="number"
+                                                />
+                                            </div>
+                                        </div>
+
+                                        <div className="col-md-12">
+                                            <div className="form-group">
+                                                <label>Brief</label>
+                                                <textarea
+                                                    className="form-control"
+                                                    name="textarea"
+                                                    id="textarea" cols="30"
+                                                    rows="3"
+                                                    placeholder="Brief"
+                                                    value={aboutSBrief || ""}
+                                                    onChange={e => setBrief(e.target.value)}
+                                                />
+                                            </div>
+                                        </div>
+
+                                        <div className="col-md-12">
+                                            <div className="row">
+                                                {attributes && Array.from(attributes).map(attrbute => {
+                                                    return (
+                                                        <div className="col-md-6" key={uid()}>
+                                                            <Attribute
+                                                                info={attrbute}
+                                                            />
+                                                        </div>
+                                                    )
+                                                })}
+                                            </div>
+                                        </div>
 
 
-                            <div className="col-md-12">
-                                <div className="d-flex align-items-center">
-                                    <button className="btn btn-primary px-3 float-right"
-                                            type={"submit"}>Save
-                                    </button>
-                                </div>
+                                        <div className="col-md-12">
+                                            <div className="d-flex align-items-center">
+                                                <button className="btn btn-primary px-3 float-right"
+                                                        type={"submit"}>Save
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+
                             </div>
                         </div>
-                    </form>
-
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-</div>
-</>
-);
+        </>
+    );
 }
 
 export default Index;
