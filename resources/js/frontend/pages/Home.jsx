1a2368e489f967630105090c9cd0e1570c8b5c8f
gmfaruk <gmfaruk2021@gmail.com>
2023-09-09 19:57:57 +0600

    update services

--- resources/js/frontend/pages/Home.jsx
+++ resources/js/frontend/pages/Home.jsx
@@ -3,30 +3,29 @@ import HeaderMeta from "../../ui/HeaderMeta.jsx";
 import Hero from "../components/Hero";
 import HomeAbout from "../components/HomeAbout";
 import {useDispatch, useSelector} from "react-redux";
-import {fetchSeoData} from "../../featurs/SEOSlice/SEOSlice.js";
 import Industry from "../components/Industry";
 import Technology from "../components/Technology";
 import Partner from "../components/Partner";
 import Client from "../components/Client";
 import Preloader from "../components/Preloader/index.jsx";
+import Service from "../components/Services/Index.jsx";
 
 function Home(props) {
     const {isLoading} = useSelector(state => state.generalSettings)
-    return (
-        <>
+    return (<>
             {isLoading && <Preloader/>}
-            <HeaderMeta
-                title="Home"
-                page="Home"
-            />
+        <HeaderMeta
+            title="Home"
+            page="Home"
+        />
             <Hero/>
             <Partner/>
             <HomeAbout/>
             <Industry/>
             <Technology/>
             <Client/>
-        </>
-    );
+            <Service/>
+        </>);
 }
 
 export default Home;
