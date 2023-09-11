7a0770b79d3ad56cceba3a6b6ba9dabfb1655e07
Zakir  Hossain <hossain.zakir@ssgbd.com>
2023-09-11 13:55:21 +0600

    complete pagination

--- resources/js/ui/Pagination.jsx
+++ resources/js/ui/Pagination.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactPaginate from "react-paginate";
+import Style from "./Pagination.module.css"
 
 function Pagination({handlePageClick, pageCount, range}) {
+    useEffect(()=>{
+        document.querySelector("li.selected").classList.add(Style.selected)
+    })
     return (
         <ReactPaginate
+            className={Style.paginationPart}
             breakLabel="..."
             nextLabel=">"
             previousLabel="<"
