7ff08b5815822c2ffdef64f11360862d9d733f6a
gmfaruk <gmfaruk2021@gmail.com>
2023-10-25 09:59:12 +0600

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,34 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        .float {
+            position: fixed;
+            width: 60px;
+            height: 60px;
+            bottom: 40px;
+            left: 40px;
+            background-color: #25d366;
+            color: #FFF;
+            border-radius: 50px;
+            text-align: center;
+            font-size: 30px;
+            box-shadow: 2px 2px 3px #999;
+            z-index: 100;
+        }
+
+        .my-float {
+            margin-top: 16px;
+        }
+    </style>
     @viteReactRefresh
     @vite('resources/js/app.jsx')
 </head>
 <body>
 <div id="app"></div>
+
+<a href="https://api.whatsapp.com/send?phone=01755597770&text=Hi, I want to know more about Bizz Solutions. Can you help me, please?"
+   class="float" target="_blank"> <i class="fa fa-whatsapp my-float"></i></a>
 
 <div id="fb-root"></div>
 
